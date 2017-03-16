5f16cf5ec2bd5db5feeb21baa23a2e9f45d70152
Bilger Yahov <bilger@studyportals.com>
2017-03-16 11:06:03 +0100

    Implement a stack.

---
+++ stack_implementation.cpp
@@ -0,0 +1,58 @@
+#include<stdio.h>
+#define MAX 10
+
+typedef int data;
+data stack[MAX];
+int top;
+
+void init(){
+	top = 0;
+}
+
+void push(data i){
+	if(top == MAX){
+		printf("The stack is full! \n");
+	}
+	else{
+		stack[top++] = i;
+	}
+}
+
+data pop(void){
+	if(top == 0){
+		printf("The stack is empty! \n");
+	}
+	else{
+		return stack[--top];
+	}
+}
+
+int isEmpty(void){
+	return (top == 0);
+}
+
+int main(void){
+	data number;
+	init();
+	
+	printf("Enter a whole number: ");
+	scanf("%d", &number);
+	
+	// Read values and enter into the stack
+	// until you receive a zero.
+	while(number != 0){
+		push(number);
+		printf("Enter a whole number: ");
+		scanf("%d", &number);
+	}
+	
+	printf("You entered: \n");
+	
+	// Remove and output all elements from the stack.
+	while(!isEmpty()){
+		printf("%d\n", pop());
+	}
+	
+	printf("\n");
+	return 0;
+}
