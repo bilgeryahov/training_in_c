b7d1c0ed52771451f50fb26357a80d8c9c4af55f
Bilger Yahov <bilger@studyportals.com>
2017-03-17 11:30:30 +0100

    Intro to pointers.

---
+++ pointers.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(){
+	// Actual variable declaration.
+	int var = 20;
+	
+	// Pointer variable declaration.
+	int *ip;
+	
+	// Store the address of var in the pointer variable.
+	ip = &var;
+	
+	// Print the address of var using itself.
+	printf("The address of var using itself is %x\n", &var);
+	
+	// Print the address of var using the pointer variable.
+	printf("The address of var using the pointer variable is %x\n", ip);
+	
+	// Access the value of var using the pointer variable.
+	printf("The value of var using the pointer variable is %d\n", *ip);
+	
+	return 0;
+}
