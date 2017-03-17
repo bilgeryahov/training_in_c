8ee799ce214ed603f8da7f8a5932ce668ce44700
Bilger Yahov <bilger@studyportals.com>
2017-03-17 14:56:50 +0100

    Add some explanations (clarifications) to pointers.

--- double_pointer.cpp
+++ double_pointer.cpp
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
+// Here *pointer means that the parameter that this function will expect will be a pointer. 
+// Basically this is the meaning of the star.
 void changeViaPointer(int *pointer);
+
+// Pointer to a pointer.
 void changeViaPointerInBetween(int **pointer);
 
 int main(){
@@ -8,17 +12,27 @@ int main(){
 	int number;
 	number = 20;
 	
+	// Here *pointer means that the variable that is declared will be a pointer.
 	int *pointer;
+	
+	// Actually asigning value to the variable.
 	pointer = &number;
 	
+	// Pointer to a pointer.
 	int **pointerInBetween;
+	
+	// Assigning value to the pointer of a pointer. 
+	// Assigning the memory location where this pointer points to. 
+	// So this is a pointer in between.
 	pointerInBetween = &pointer;
 	
 	printf("The number before changing is %d\n", number);
 	
+	// Pass the pointer variable.
 	changeViaPointer(pointer);
 	printf("The number after pointer changing is %d\n", number);
 	
+	// Pass the pointer of a pointer variable.
 	changeViaPointerInBetween(pointerInBetween);
 	printf("The number after pointer in between changing is %d\n", number);
 	
@@ -26,6 +40,11 @@ int main(){
 }
 
 void changeViaPointer(int *pointer){
+	
+	// Okay, at this point we have received a variable called pointer,
+	// which points to some value. In order to access this value
+	// we need to use *pointer.
+	// BUT THIS IS DIFFERENT FROM THE *pointer IN THE FUNCTION DECLARATION!!!
 	*pointer = *pointer + 20;
 }
 
